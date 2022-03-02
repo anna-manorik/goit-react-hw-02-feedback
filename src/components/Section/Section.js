88b76147957686c20cf7ce9a68dbe51564033744
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-03-02 21:31:31 +0200

    Update Section.js

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import styles from './section.module.css';
 
 const Section = ({ title, children }) => (
-    <div>
-        <p className={styles.title}>{title}</p>
-        {children}
-    </div>
+  <div>
+    <p className={styles.title}>{title}</p>
+    {children}
+  </div>
 );
 
 Section.propType = {
-    title: PropTypes.string,
-    children: PropTypes.element,
-}
+  title: PropTypes.string,
+  children: PropTypes.element,
+};
 
 export default Section;
